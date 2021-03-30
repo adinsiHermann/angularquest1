b182a8010cad3a325f75d8e94e182d39636504ef
hermann <adinsihermann1@gmail.com>
2021-03-30 16:45:24 +0200

    Envoie dépôt exo quest 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myquest1';
+  title = 'Bienvenue sur le site de Hermann';
 }
